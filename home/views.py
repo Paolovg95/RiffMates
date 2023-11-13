ae8d50b4800056fc763fa6c12dcda0890cebba28
Paolo <paolovargas1@Paolos-MacBook-Pro.local>
2023-11-12 22:30:12 -0300

    Credit view, template and URL added

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_credits(request):
+    content = "Paolo & Nicky"
+    return render(request, "credits.html", {"content": content})
