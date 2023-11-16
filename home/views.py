8426398a331f1e46e7aa2442abef536c26998b30
Paolo <paolovargas1@Paolos-MacBook-Pro.local>
2023-11-16 10:32:36 -0300

    Credits View using passing Context argument simulating Model objects

--- home/views.py
+++ home/views.py
@@ -3,15 +3,27 @@ from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 
+# Render template w/ context via template
 def view_credits(request):
-    content = "Paolo & Nicky"
-    return render(request, "credits.html", {"content": content})
 
+    credit_context = {
+        "credit_names": ['Paolo', 'Nicky'],
+        "page_name": "Credits page"
+    }
+    context = {
+        "object": credit_context
+    }
+
+    return render(request, "credits.html", context)
+
+# Render Http Response
 def view_about(request):
     code = HttpResponse.status_code
     content = f"This is about view. Code: {code}"
     return HttpResponse(content,content_type="text/plain")
+
+
 def view_version(request):
     code = HttpResponse.status_code
-    version = JsonResponse({ "Version": "1.0.0","code": code})
-    return HttpResponse(version, content_type='application/json.')
+    data = { "Version": "1.0.0","code": code}
+    return JsonResponse(data)
