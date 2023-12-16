70962a81245043d75dd302e3ba55c23b6e22ac96
paolovg95 <paolo9517@gmail.com>
2023-12-16 09:11:06 -0300

    Added str method to print out object info

--- bands/models.py
+++ bands/models.py
@@ -6,3 +6,5 @@ class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
+    def __str__(self):
+        return f"Musician (id={self.id}, last_name={self.last_name})"
