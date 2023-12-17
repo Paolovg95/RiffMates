b0f8ac09b01df1dbbb06f6e23ded4b922abe68c1
paolovg95 <paolo9517@gmail.com>
2023-12-17 14:45:20 -0300

    first name added to __str__ return object info

--- bands/models.py
+++ bands/models.py
@@ -7,4 +7,4 @@ class Musician(models.Model):
     last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
     def __str__(self):
-        return f"Musician (id={self.id}, last_name={self.last_name})"
+        return f"Musician (id={self.id}, first_name={self.first_name}last_name={self.last_name})"
