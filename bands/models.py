a58af7ab21332631a34eb1938fe145c19fbb7e58
paolovg95 <paolo9517@gmail.com>
2023-12-20 01:09:08 -0300

    Venue and Room models added, one-to-many relation

--- bands/models.py
+++ bands/models.py
@@ -8,3 +8,9 @@ class Musician(models.Model):
     birth_date = models.DateField()
     def __str__(self):
         return f"Musician (id={self.id}, first_name={self.first_name}last_name={self.last_name})"
+class Venue(models.Model):
+    name = models.CharField(max_length=20)
+
+class Room(models.Model):
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    room_name = models.CharField(max_length=20)
