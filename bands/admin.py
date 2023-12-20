6dede40aa8da58f604308143570ea45ebfaf8f38
paolovg95 <paolo9517@gmail.com>
2023-12-20 13:46:42 -0300

    Models: Venue, Rooms added to Admin panel

--- bands/admin.py
+++ bands/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from bands.models import Musician
+from bands.models import Musician, Venue, Room
 # Register your models here.
 admin.site.register(Musician)
+admin.site.register(Room)
+admin.site.register(Venue)
