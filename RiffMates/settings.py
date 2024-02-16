bb23e2b6f109da831987e75f7ad8605ae02c9000
paolovg95 <paolo9517@gmail.com>
2024-02-16 00:29:17 -0300

    Added settings for media upload and urls.py config to serve files using static()

--- RiffMates/settings.py
+++ RiffMates/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# RiffMates/RiffMates/settings.py
+MEDIA_ROOT = BASE_DIR.parent / 'outside/RiffMates/uploads'
+MEDIA_URL = '/media/'
--- RiffMates/urls.py
+++ RiffMates/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# RiffMates/RiffMates/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from home import views as home_views
@@ -31,3 +34,7 @@ urlpatterns = [
     path('venues/', get_venues, name="venues"),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
